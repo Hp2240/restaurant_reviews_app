8d5926993166c584468933fa1f0cfb3f4baedd0c
Hyeree Park <hp2240@nyu.edu>
2022-08-01 19:17:28 -0400

    building out initial heroku connection

--- server.js
+++ server.js
@@ -8,4 +8,8 @@ app.get('/', (req, res) => {
   res.send('root route hit')
 })
 
+app.get('/*', (req, res) => {
+  res.sendFile(`${__dirname}/client/build/index.html`)
+})
+
 app.listen(PORT, () => console.log('server is running at PORT', PORT))
