653b535023dfe49b77604f080e1b15ee16fb165e
Hyeree Park <hp2240@nyu.edu>
2022-08-01 20:21:35 -0400

    seccessful db query for restaurants

--- server.js
+++ server.js
@@ -4,9 +4,15 @@ const db = require('./db')
 
 const app = express()
 
-// routes
-app.get('/restaurants', (req, res) => {
-  res.send('root route hit')
+const { Restaurant } = require('./models')
+
+// MIDDLEWARE
+app.use(express.static(`${__dirname}/client/build`))
+
+// ROUTES
+app.get('/restaurants', async (req, res) => {
+  let restaurants = await Restaurant.find({})
+  res.send(restaurants)
 })
 
 app.get('/*', (req, res) => {
