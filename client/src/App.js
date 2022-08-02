6468f4f8a83eb3b59cdae015bd4e2e3960b99f8a
Hyeree Park <hp2240@nyu.edu>
2022-08-02 11:14:46 -0400

    setup routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,12 @@
 import './App.css'
 import Nav from './components/Nav'
 import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import RestaurantList from './pages/RestaurantList'
+import RestaurantForm from './pages/RetaurantForm'
+import ReviewDetails from './pages/ReviewDetails'
+import RestaurantDetails from './pages/RestaurantDetails'
+import ReviewForm from './pages/ReviewForm'
 
 const App = () => {
   return (
@@ -10,12 +16,12 @@ const App = () => {
       </header>
       <main>
         <Routes>
-          <Route></Route>
-          <Route></Route>
-          <Route></Route>
-          <Route></Route>
-          <Route></Route>
-          <Route></Route>
+          <Route index element={<Home />} />
+          <Route path="/restaruants" element={<RestaurantList />} />
+          <Route path="/add" element={<RestaurantForm />} />
+          <Route path="/review/:id" element={<ReviewDetails />} />
+          <Route path="/restaurants/:id" element={<RestaurantDetails />} />
+          <Route path="restaurants/:id/reviews" element={<ReviewForm />} />
         </Routes>
       </main>
     </div>
---
+++ client/src/pages/RestaurantForm.js
@@ -0,0 +1,2 @@
+const RestaurantForm = () => {}
+export default RestaurantForm
