77e1642b2bd70ea0ef8818bb95c44f45d25e4dd0
Hyeree Park <hp2240@nyu.edu>
2022-08-01 23:31:48 -0400

    adding middleware for use from the client

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const cors = require('cors')
+const logger = require('morgan')
 const PORT = process.env.PORT || 3001
 const routes = require('./routes')
 const db = require('./db')
@@ -10,6 +12,8 @@ const { Restaurant } = require('./models')
 // MIDDLEWARE
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+app.use(cors())
+app.use(logger('dev'))
 app.use(express.static(`${__dirname}/client/build`))
 
 // ROUTES
