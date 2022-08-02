ab7926ffd348c9b2f79368d4bc7580878345007e
Hyeree Park <hp2240@nyu.edu>
2022-08-01 23:12:16 -0400

    refactored to controllers and routers

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const PORT = process.env.PORT || 3001
+const routes = require('./routes')
 const db = require('./db')
 
 const app = express()
@@ -12,23 +13,6 @@ app.use(express.urlencoded({ extended: false }))
 app.use(express.static(`${__dirname}/client/build`))
 
 // ROUTES
-// get all restaurants
-app.get('/restaurants', async (req, res) => {
-  let restaurants = await Restaurant.find({})
-  res.send(restaurants)
-})
-
-// create a restaurant
-app.post('/restaurants', async (req, res) => {
-  let createdRestaurant = await Restaurant.create(req.body)
-  res.send(createdRestaurant)
-})
-
-// get a single restaurant
-app.get('/restaurants/:id', async (req, res) => {
-  let foundRestaurant = await Restaurant.findById(req.params.id)
-  res.send(foundRestaurant)
-})
 
 app.get('/*', (req, res) => {
   res.sendFile(`${__dirname}/client/build/index.html`)
